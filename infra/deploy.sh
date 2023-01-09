8661a5ed7aeabdd17cdeb26f764c1778e6a69ae3
asadagaha <dongon74@gmail.com>
2023-01-09 09:27:16 +0900

    test

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -5,5 +5,6 @@ COMMAND=$1
 WORKSPACE=$2
 OPTIONS=${@:3}
 
+terraform fmt -recursive
 terraform workspace select ${WORKSPACE}
 terraform ${COMMAND} -var-file=.terraform/${WORKSPACE}.tfvars ${OPTIONS}
